630fdd495daaf88769802a0d84f891bdcc33c5d8
Dmitry Taras <wellbiz@ya.ru>
2022-01-17 21:56:23 +0300

    добавил сортировку в телефонную книгу

--- phonebook/js/script.js
+++ phonebook/js/script.js
@@ -293,6 +293,35 @@ const data = [
             form.reset();
         });
     };
+
+    const sortTh = (colNum) => {
+        let tbody = document.querySelector('.table tbody');
+
+        let rowsArray = Array.from(tbody.rows);
+
+        // compare(a, b) сравнивает две строки, нужен для сортировки
+        let compare;
+
+        compare = function (rowA, rowB) {
+            return rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML
+                ? 1
+                : -1;
+        };
+
+        // сортировка
+        rowsArray.sort(compare);
+
+        tbody.append(...rowsArray);
+    };
+    const sortingbyTH = () => {
+        const table = document.querySelector('.table');
+        table.addEventListener('click', (e) => {
+            if (e.target.tagName != 'TH') return;
+
+            let th = e.target;
+            sortTh(th.cellIndex, th.dataset.type);
+        });
+    };
     const init = (selectorApp, title) => {
         const app = document.querySelector(selectorApp);
 
@@ -316,6 +345,7 @@ const data = [
         }, 2000);
 
         //добавить сортировку по алфавиту
+        sortingbyTH();
     };
     window.phoneBookInit = init;
 }
